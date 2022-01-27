a22f729a7ccd66fce91b55a080638693c87954e1
andrey12v <andrey12v@gmail.com>
2022-01-26 21:55:10 -0600

    Add MealItemForm and Input components
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599472#overview

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -11,7 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
