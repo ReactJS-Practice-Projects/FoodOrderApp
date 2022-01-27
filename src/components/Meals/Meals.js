d640131650792c2aa64bcfb16d49fa374218427e
andrey12v <andrey12v@gmail.com>
2022-01-26 21:35:44 -0600

    Add MealItem Component to render individual items
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599466#overview

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -3,8 +3,6 @@ import { Fragment } from 'react';
 import MealsSummary from './MealsSummary';
 import AvailableMeals from './AvailableMeals';
 
-//we use fragment to return side by side (sibling) elements since react always requires root element
-//that's why we use fragment to return sibling elements
 const Meals = () => {
   return (
     <Fragment>
