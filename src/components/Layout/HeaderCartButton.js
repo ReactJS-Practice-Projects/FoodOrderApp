66f5be3ee124e2ef8c7973d29c85fb12f5117478
andrey12v <andrey12v@gmail.com>
2022-01-31 22:46:25 -0600

    Add UseEffect() with bump class to the Header Cart Button
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25599520#overview

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,47 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  //get items from the context in order to know if a new item was added to the cart 
+  //and as a result of it display effect
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  //here we assign additional class bump to teh button if btnIsHighlighted is true
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  //if we don't have any items in the cart 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    //if the number of items is changed then we set the state variable setBtnIsHighlighted to true and 
+    //add the class bump to the button
+    setBtnIsHighlighted(true);
+
+    //then after 300 miliseconds we set the state variable setBtnIsHighlighted back to false and
+    //as a resu;lt of it remove bump class from thr button 
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    //here we just clean timer
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
